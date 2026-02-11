b454c69d1f42313c9fa261bccc56f3bd1696c55e
Ithran Einhorn <ithran_einhorn@psfc.coop>
2026-02-11 15:25:52 -0500

    Support for nested loops, repeating segments and conditional elements.

--- pythonedi/EDIGenerator.py
+++ pythonedi/EDIGenerator.py
@@ -22,13 +22,14 @@ class EDIGenerator(object):
         if "ST" not in data:
             Debug.explain(supported_formats["ST"])
             raise ValueError("No transaction set header found in data.")
-        ts_id = data["ST"][0]
-        if ts_id not in supported_formats:
+        # PSFC: Made ts_id an instance variable
+        self.ts_id = data["ST"][0]
+        if self.ts_id not in supported_formats:
             raise ValueError("Transaction set type '{}' is not supported. Valid types include: {}".format(
-                ts_id,
+                self.ts_id,
                 "".join(["\n - " + f for f in supported_formats])
             ))
-        edi_format = supported_formats[ts_id]
+        edi_format = supported_formats[self.ts_id]
 
         output_segments = []
 
@@ -36,51 +37,95 @@ class EDIGenerator(object):
         for section in edi_format:
             if section["type"] == "segment":
                 if section["id"] not in data:
-                    if section["req"] == "O":
-                        # Optional segment is missing - that's fine, keep going
+                    if section["req"] in ("O", "C"):
+                        # Optional or conditional segment is missing - that's fine, keep going
                         continue
                     elif section["req"] == "M":
                         # Mandatory segment is missing - explain it and then fail
                         Debug.explain(section)
                         raise ValueError("EDI data is missing mandatory segment '{}'.".format(section["id"]))
                     else:
-                        raise ValueError("Unknown 'req' value '{}' when processing format for segment '{}' in set '{}'".format(section["req"], section["id"], ts_id))
-                output_segments.append(self.build_segment(section, data[section["id"]]))
+                        raise ValueError("Unknown 'req' value '{}' when processing format for segment '{}' in set '{}'".format(section["req"], section["id"], self.ts_id))
+                output_segments.extend(self.build_segment_list(section, data[section["id"]]))
             elif section["type"] == "loop":
-                if section["id"] not in data:
-                    mandatory = [segment for segment in section["segments"] if segment["req"] == "M"]
-                    if len(mandatory) > 0:
-                        Debug.explain(section)
-                        raise ValueError("EDI data is missing loop {} with mandatory segment(s) {}".format(section["id"], ", ".join([segment["id"] for segment in mandatory])))
-                    else:
-                        # No mandatory segments in loop - continue
-                        continue
-                # Verify loop length
-                if len(section["segments"]) > section["repeat"]:
-                    raise ValueError("Loop '{}' has {} segments (max {})".format(section["id"], len(section["segments"]), section["repeat"]))
-                # Iterate through and build segments in loop
-                for iteration in data[section["id"]]:
-                    for segment in section["segments"]:
-                        if segment["id"] not in iteration:
-                            if segment["req"] == "O":
-                                # Optional segment is missing - that's fine, keep going
-                                continue
-                            elif segment["req"] == "M":
-                                # Mandatory segment is missing - explain loop and then fail
-                                Debug.explain(section)
-                                raise ValueError("EDI data in loop '{}' is missing mandatory segment '{}'.".format(section["id"], segment["id"]))
-                            else:
-                                raise ValueError("Unknown 'req' value '{}' when processing format for segment '{}' in set '{}'".format(segment["req"], segment["id"], ts_id))
-                        output_segments.append(self.build_segment(segment, iteration[segment["id"]]))
+                # PSFC: Allow for nested loops
+                output_segments.extend(self.build_loop_list(section, data))
 
-        return self.segment_delimiter.join(output_segments)
+        # PSFC: Need trailing segment delimiter
+        return self.segment_delimiter.join(output_segments) + self.segment_delimiter
+
+    def build_loop_list(self, section, data):
+        loop_segments = []
+
+        if section["id"] not in data:
+            mandatory = [segment for segment in section["segments"] if segment["req"] == "M"]
+            if len(mandatory) > 0:
+                Debug.explain(section)
+                raise ValueError("EDI data is missing loop {} with mandatory segment(s) {}".format(section["id"], ", ".join([segment["id"] for segment in mandatory])))
+            else:
+                # No mandatory segments in loop - continue
+                return loop_segments
+
+        # Verify loop length
+        if len(section["segments"]) > section["repeat"]:
+            raise ValueError("Loop '{}' has {} segments (max {})".format(section["id"], len(section["segments"]), section["repeat"]))
+        # Iterate through and build segments in loop
+        for iteration in data[section["id"]]:
+            for segment in section["segments"]:
+                if segment["id"] not in iteration:
+                    if section["req"] in ("O", "C"):
+                        # Optional or conditional segment is missing - that's fine, keep going
+                        continue
+                    elif segment["req"] == "M":
+                        # Mandatory segment is missing - explain loop and then fail
+                        Debug.explain(section)
+                        raise ValueError("EDI data in loop '{}' is missing mandatory segment '{}'.".format(section["id"], segment["id"]))
+                    else:
+                        raise ValueError("Unknown 'req' value '{}' when processing format for segment '{}' in set '{}'".format(segment["req"], segment["id"], self.ts_id))
+
+                if segment["type"] == "loop":
+                    # PSFC: Process nested loop
+                    loop_segments.extend(self.build_loop_list(segment, iteration))
+                else:
+                    loop_segments.extend(self.build_segment_list(segment, iteration[segment["id"]]))
+
+        return loop_segments
+
+    def build_segment_list(self, segment, segment_data):
+        segment_list = []
+
+        if not isinstance(segment_data[0], list):
+            # PSFC: Single occurrence of segment
+            segment_list.append(self.build_segment(segment, segment_data))
+        else:
+            # PSFC: Multiple occurrences of segment
+            num_uses = len(segment_data)
+            # PSFC: If max_uses not present, or -1, then unbounded
+            max_uses = segment.get('max_uses', -1)
+
+            if max_uses > -1 and num_uses > max_uses:
+                raise ValueError("Segment '{}' may not repeat more than {} time(s), found: {}".format(segment["id"], max_uses, num_uses))
+
+            for segment_entry in segment_data:
+                if isinstance(segment_entry, list):
+                    segment_list.append(self.build_segment(segment, segment_entry))
+                else:
+                    raise TypeError("Repeated segment '{}' must have elements in list, found: '{}'".format(segment["id"], type(segment_entry)))
+
+        return segment_list
 
     def build_segment(self, segment, segment_data):
         # Parse segment elements
         output_elements = [segment["id"]]
-        for e_data, e_format, index in zip(segment_data, segment["elements"], range(len(segment["elements"]))):
-            output_elements.append(self.build_element(e_format, e_data))
-        
+
+        # PSFC: Exception handling to report segment for failed element
+        try:
+            for e_data, e_format, index in zip(segment_data, segment["elements"], range(len(segment["elements"]))):
+                # PSFC: Allow for composite elements
+                output_elements.append(self.build_element_list(e_format, e_data))
+        except ValueError as ve:
+            raise ValueError("{}, in segment: {}".format(ve, segment['id']))
+
         # End of segment. If segment has syntax rules, validate them.
         if "syntax" in segment:
             for rule in segment["syntax"]:
@@ -121,14 +166,32 @@ class EDIGenerator(object):
                                 break
                             elif output_elements[idx] != "":
                                 found += 1
-                        if 0 < found < len(rule["criteria"]):
-                            # Some but not all the elements are present
+                        # PSFC: IFATLEASTONE satisfied if any elements found
+                        if found == 0:
+                            # None of the other elements were found
                             first_element = "{}{:02d}".format(segment["id"], rule["criteria"][0])
-                            required_elements = ", ".join(["{}{:02d}".format(segment["id"], e) for e in rule["criteria"][0]])
+                            # PSFC: Fixed typo, was dereferencing rule["criteria"][0]
+                            required_elements = ", ".join(["{}{:02d}".format(segment["id"], e) for e in rule["criteria"]])
                             Debug.explain(segment)
                             raise ValueError("Syntax error parsing segment {}: If {} is present, at least one of {} are required.".format(segment["id"], first_element, required_elements))
-            
+
+        # PSFC: Remove trailing empty elements
+        while output_elements and not output_elements[-1]:
+            output_elements = output_elements[:-1]
+
         return self.element_delimiter.join(output_elements)
+
+    def build_element_list(self, e_format, e_data):
+        element_type = e_format["type"]
+
+        if element_type == "element":
+            return self.build_element(e_format, e_data)
+        elif element_type == "composite":
+            formatted_elements =\
+                [ self.build_element(ce_format, ce_data) for ce_data, ce_format in zip(e_data[e_format["id"]], e_format["elements"]) ]
+            return self.component_element_delimiter.join(formatted_elements)
+        else:
+            raise ValueError("Element {} ({}) has unknown type '{}'".format(e_format["id"], e_format["name"], element_type))
 
     def build_element(self, e_format, e_data):
         element_id = e_format["id"]
@@ -136,10 +199,11 @@ class EDIGenerator(object):
         if e_data is None:
             if e_format["req"] == "M":
                 raise ValueError("Element {} ({}) is mandatory".format(element_id, e_format["name"]))
-            elif e_format["req"] == "O":
+            # PSFC: allow conditional req value
+            elif e_format["req"] in ("O", "C"):
                 return ""
             else:
-                raise ValueError("Unknown 'req' value '{}' when processing format for element '{}' in set '{}'".format(e_format["req"], element_id, ts_id))
+                raise ValueError("Unknown 'req' value '{}' when processing format for element '{}' in set '{}'".format(e_format["req"], element_id, self.ts_id))
         try:
             if e_format["data_type"] == "AN":
                 formatted_element = str(e_data)
@@ -149,13 +213,13 @@ class EDIGenerator(object):
                 elif e_format["length"]["max"] == 6:
                     formatted_element = e_data.strftime("%y%m%d")
                 else:
-                    raise ValueError("Invalid length ({}) for date field in element '{}' in set '{}'".format(e_format["length"], element_id, ts_id))
+                    raise ValueError("Invalid length ({}) for date field in element '{}' in set '{}'".format(e_format["length"], element_id, self.ts_id))
             elif e_format["data_type"] == "TM":
                 if e_format["length"]["max"] in (4, 6, 7, 8):
                     #formatted_element = e_data.strftime("%H%M%S%f")
                     formatted_element = e_data.strftime("%H%M")
                 else:
-                    raise ValueError("Invalid length ({}) for time field in element '{}' in set '{}'".format(e_format["length"], element_id, ts_id))
+                    raise ValueError("Invalid length ({}) for time field in element '{}' in set '{}'".format(e_format["length"], element_id, self.ts_id))
             elif e_format["data_type"] == "R":
                 formatted_element = str(float(e_data))
             elif e_format["data_type"].startswith("N"):
